1ee3a921755615049c305e60c87c680f278b514e
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-09 10:40:16 +0200

    ejercicio 14

---
+++ 14_ejercicio_runnable/src/principal/Lanzador.java
@@ -0,0 +1,40 @@
+package principal;
+
+import java.util.Scanner;
+
+import service.Numeros;
+
+public class Lanzador {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un número: ");
+		int numero1 = sc.nextInt();
+		System.out.println("Introduce un número: ");
+		int numero2 = sc.nextInt();
+		System.out.println("Introduce un número: ");
+		int numero3 = sc.nextInt();
+		
+		Numeros n1 = new Numeros(numero1);
+		Numeros n2 = new Numeros(numero2);
+		Numeros n3 = new Numeros(numero3);
+		
+		new Thread(n1).start();
+		new Thread(n2).start();
+		new Thread(n3).start();
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println("haciendo otras cosas");
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ 14_ejercicio_runnable/src/service/Numeros.java
@@ -0,0 +1,26 @@
+package service;
+
+public class Numeros implements Runnable {
+	
+	int numero;
+	
+	public Numeros(int numero) {
+		this.numero = numero;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+				System.out.println(numero + " x " + i +" = "+ numero*i);
+			
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
