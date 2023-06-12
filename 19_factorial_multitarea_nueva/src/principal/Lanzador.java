18263a741e2ad5c76180a6e7644e1912f82c290d
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-12 09:56:35 +0200

    nuevo ejercicio

--- 19_factorial_multitarea_nueva/src/principal/Lanzador.java
+++ 19_factorial_multitarea_nueva/src/principal/Lanzador.java
@@ -1,17 +1,27 @@
 package principal;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import tareas.TareaFactorial;
 
 public class Lanzador {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		ExecutorService executor=Executors.newCachedThreadPool();
-		executor.submit(new TareaFactorial(4));
-		executor.submit(new TareaFactorial(8));
-		executor.submit(new TareaFactorial(6));
+		Future<Long> f1= executor.submit(new TareaFactorial(4));
+		Future<Long> f2=executor.submit(new TareaFactorial(8));
+		Future<Long> f3=executor.submit(new TareaFactorial(6));
+		// mientras haya alguna en ejecución, hacemos las tareas del main
+		while(!f1.isDone() || !f2.isDone() || !f3.isDone()){
+			System.out.println("Haciendo algo...");
+		}
+		// llega a ese punto cuando las tres tareas han finalizado
+		System.out.println("Factorial del 4: " + f1.get());
+		System.out.println("Factorial del 8: " + f2.get());
+		System.out.println("Factorial del 6: " + f3.get());
 		
 		executor.shutdown();
 
