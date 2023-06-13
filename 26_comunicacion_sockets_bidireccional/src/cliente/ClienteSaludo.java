1b1dd953c8a5ef517f5db789e41de3faada4dd8a
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-13 14:19:34 +0200

    ejercicio 26 y 27

--- 26_comunicacion_sockets_bidireccional/src/cliente/ClienteSaludo.java
+++ 26_comunicacion_sockets_bidireccional/src/cliente/ClienteSaludo.java
@@ -3,6 +3,7 @@ package cliente;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class ClienteSaludo {
@@ -10,10 +11,13 @@ public class ClienteSaludo {
 	public static void main(String[] args) {
 	
 		
-		try(Socket sc = new Socket("localhost", 8000);) {
+		try(Socket sc = new Socket("localhost", 8000);
+				PrintStream out = new PrintStream(sc.getOutputStream());
 		// lectura del mensaje enviado desde el servidor
-		BufferedReader bf = new BufferedReader(new InputStreamReader(sc.getInputStream()));
-		System.out.println("El servidor envia: " + bf.readLine());
+		BufferedReader bf = new BufferedReader(new InputStreamReader(sc.getInputStream())))
+		{
+			out.println("profe");
+			System.out.println("El servidor envia: " + bf.readLine());
 		}
 		catch (IOException ex) {
 			ex.printStackTrace();
--- 26_comunicacion_sockets_bidireccional/src/servidor/HiloCliente.java
+++ 26_comunicacion_sockets_bidireccional/src/servidor/HiloCliente.java
@@ -1,6 +1,8 @@
 package servidor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
@@ -12,8 +14,10 @@ public class HiloCliente implements Runnable {
 	
 	@Override
 	public void run() {
-		try(PrintStream out  = new PrintStream(sc.getOutputStream())){
-			out.println("hola");
+		try(PrintStream out  = new PrintStream(sc.getOutputStream());
+				BufferedReader bf = new BufferedReader(new InputStreamReader(sc.getInputStream()))){
+			
+			out.println("hola " + bf.readLine());
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
