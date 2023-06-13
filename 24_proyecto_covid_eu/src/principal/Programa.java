85a1ff2c62a1e0e7ed52fd9cbb916df331c5f84c
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-13 11:51:44 +0200

    ejercicio covid

--- 24_proyecto_covid_eu/src/principal/Programa.java
+++ 24_proyecto_covid_eu/src/principal/Programa.java
@@ -1,9 +1,21 @@
 package principal;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import model.Item;
+import service.CovidService;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CovidService covidService = new CovidService();
+		ExecutorService executor=Executors.newCachedThreadPool();
+		CompletableFuture<Long> cf = CompletableFuture.supplyAsync(()->covidService.casosTotales(), executor);
+		cf.whenCompleteAsync((r, ex) -> System.out.println("Casos totales: " + r));
+		covidService.incidenciaMediaPaises().forEach((i, m) -> System.out.println(i +" , " + m));
+		executor.shutdown();
 
 	}
 
--- 24_proyecto_covid_eu/src/service/CovidService.java
+++ 24_proyecto_covid_eu/src/service/CovidService.java
@@ -6,10 +6,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 
@@ -20,67 +22,63 @@ public class CovidService {
 	
 	String http="https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/json/";
 	
-	private List<Item> itemsFromJson(){
-		Gson gson = new Gson();
+	private Stream<Item> itemsFromJson(){
+		Gson gson=new Gson();
 		try {
-			HttpRequest request = HttpRequest.newBuilder()
-									.uri(URI.create(http))
-									.GET()
-									.build();
-			
-			HttpClient cliente=HttpClient.newBuilder()
+			HttpRequest request=HttpRequest.newBuilder()
+								.uri(URI.create(http))
+								.GET()
 								.build();
-			
-			HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
-			return List.of(gson.fromJson(response.body(), Item[].class));
-		} catch (Exception ex) {
+			HttpClient client=HttpClient.newBuilder()
+								.build();
+			HttpResponse<String> respuesta=client.send(request, BodyHandlers.ofString());
+			return Arrays.stream(gson.fromJson(respuesta.body(), Item[].class));
+		}catch(Exception ex) {
 			ex.printStackTrace();
-			return List.of();
+			return Stream.empty();
 		}
+	}
+	public long casosTotales() {
+		LocalDate f=fechaMasReciente();
+		/*return itemsFromJson() //Stream<Item>
+		.filter(it->it.getIndicador().equals("cases")&&
+				Utilidades.convertirTextoFecha(it.getFechaMuestra()).equals(fechaMasReciente())) //Stream<Item>
+		.collect(Collectors.summingLong(it->it.getCasosAcumulados()));*/
+		 return itemsFromJson() 
+		            .filter(it -> it.getIndicador()
+		                    .equals("cases")&& FechaALocalDate.convertirTextoFecha(it.getFechaMuestra()).equals(f))
+		            .collect(Collectors.summingLong(it->it.getCasosAcumulados()));
+		
+	}
+	
+	public LocalDate fechaMasReciente() {
+		return FechaALocalDate.convertirTextoFecha(itemsFromJson() //Stream<Item>
+				.max(Comparator.comparing(it->FechaALocalDate.convertirTextoFecha(it.getFechaMuestra()))) //Optional<Item>
+				.orElse(new Item()) //Item
+				.getFechaMuestra() //String
+				);//LocalDate
 	}
 	
 	
-	public long casosTotales() {
-		
-		return itemsFromJson()
-				.stream()
-				.filter(it->it.getIndicador().equals("cases")&&FechaALocalDate.convertirTextoFecha(it.getFechaMuestra().equals(fechaMasReciente())))
-				.collect(Collectors.summingLong(it->it.getCasosAcumulados()));
-				
-				
-		}
-	
-	
+	//métodos para la incidencia media de cada pais
 	public double incidenciaMedia(String pais) {
-		
 		return itemsFromJson()
-				.stream()
-				.filter(it->it.getPais().equals(pais)&&it.getIndicador().equals("cases"))
+				.filter(it->it.getPais().equals(pais)&&it.getIndicador().equals("cases")) //Stream<Pais>
 				.collect(Collectors.averagingDouble(it->it.getIncidencia()));
+				
+				
 	}
 	
 	public List<String> paises(){
-		
 		return itemsFromJson()
-				.stream()
-				.map(c->c.getPais())
-				.collect(Collectors.toList());
+                .map(c->c.getPais())
+                .collect(Collectors.toList());
 	}
 	
-	private LocalDate fechaMasReciente() {
-		
-		return FechaALocalDate.convertirTextoFecha(itemsFromJson())
-				.max(Comparator.comparing(it->FechaALocalDate.convertirTextoFecha(it.getFechaMuestra())))
-				.orElse(new Item())
-				.getFechaMuestra());
-	}
-	
-	
-	public Map<String, Double> incidenciaPaises(){
-		
+	//otra alternativa para la incidencia media de paises
+	public Map<String,Double> incidenciaMediaPaises(){
 		return itemsFromJson()
-				.stream()
-				.filter(c->c.getIndicador().equals("cases"))
+				.filter(it->it.getIndicador().equals("cases")) //Stream<Item>
 				.collect(Collectors.groupingBy(it->it.getPais(), Collectors.averagingDouble(it->it.getIncidencia())));
 	}
 
